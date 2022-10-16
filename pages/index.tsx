05c539b367fc0024370be26b227cb46d84d09af8
masterjs <masterjs@masterjsui-Macmini.local>
2022-10-16 19:55:46 +0900

    #3.1 TailwindCSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return null; 
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-blue-500'>zero-market</h1>
+    </div>
+  ); 
 }
 
 export default Home
