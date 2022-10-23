6794215a755b7c13c65e42874ccd1a5308fc8c98
zeronedev <ok7375@gmail.com>
2022-10-24 08:05:29 +0900

    #8.5 Paths: @blis, @components

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,11 @@
+import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/server/client";
+import client from "@libs/server/client";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.status(200).end();
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
